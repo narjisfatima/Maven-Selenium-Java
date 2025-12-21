ed05cc49c6d471390c6d2bf3dbe21bea59ccf9e9
Narjis Fatima <narjisfatima987@gmail.com>
2025-12-21 11:36:12 +0530

    Add chrome options

--- selenium-framework/src/main/java/com/example/automation/driver/DriverFactory.java
+++ selenium-framework/src/main/java/com/example/automation/driver/DriverFactory.java
@@ -13,6 +13,10 @@ public class DriverFactory {
         if(driver == null){
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
             WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver(options);
             driver.manage().window().maximize();
