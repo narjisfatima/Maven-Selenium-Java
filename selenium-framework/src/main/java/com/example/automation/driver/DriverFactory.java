e866508531e41d8aca6fa6d9db819d12ea92ca87
Narjis Fatima <narjisfatima987@gmail.com>
2025-12-21 10:34:47 +0530

    Revert back code from loginPage yml and DriverFactory file

--- selenium-framework/src/main/java/com/example/automation/driver/DriverFactory.java
+++ selenium-framework/src/main/java/com/example/automation/driver/DriverFactory.java
@@ -1,7 +1,6 @@
 package com.example.automation.driver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -11,12 +10,8 @@ public class DriverFactory {
     
     public static WebDriver getDriver(){
         if(driver == null){
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--headless=new");        // Run without GUI
-            options.addArguments("--no-sandbox");          // Disable sandbox for root/containers
-            options.addArguments("--disable-dev-shm-usage"); // Avoid /dev/shm crashes
             WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver(options);
+            driver = new ChromeDriver();
             driver.manage().window().maximize();
         }
         return driver;
